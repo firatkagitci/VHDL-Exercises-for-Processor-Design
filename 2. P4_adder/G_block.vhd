16a83d375da150bc1d772441e2573bd06810f80c
Firat Kagitci <firat_kagitci@hotmail.com>
2024-09-15 01:56:17 +0200

    Automated commit on Paz 15.09.2024  1:56:17,33

--- 2. P4_adder/G_block.vhd
+++ 2. P4_adder/G_block.vhd
@@ -1,14 +1,24 @@
-library ieee; 
-use ieee.std_logic_1164.all; 
+library ieee;
+use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-entity G_BLOCK is 
+entity G_BLOCK is
 	
+	port(
+	
+	Gk1j	: in std_logic;
+	Pik 	: in std_logic;
+	Gik		: in std_logic;
+	Gij		: out std_logic;
+	
+	);
 	
 end G_BLOCK;
 
 architecture BEHAVIORAL of G_BLOCK is
 begin
+
+	Gij <= (Gk1j and Pik) or Gik;	
 
 
 end architecture BEHAVIORAL;
--- 2. P4_adder/PG_block.vhd
+++ 2. P4_adder/PG_block.vhd
@@ -6,9 +6,24 @@ use ieee.std_logic_unsigned.all;
 
 entity PG_BLOCK is 
 
+	port(
+	
+	Gk1j	: in std_logic;
+	Pk1j	: in std_logic;
+	Pik 	: in std_logic;
+	Gik		: in std_logic;
+	Pij		: out std_logic;	
+	Gij		: out std_logic;
+	
+	);
+
 end G_BLOCK;
 
 architecture BEHAVIORAL of PG_BLOCK is
 begin
-
+	
+	Pij <= Pk1j and Pik;
+	
+	Gij <= (Gk1j and Pik) or Gik;
+	
 end architecture BEHAVIORAL;
