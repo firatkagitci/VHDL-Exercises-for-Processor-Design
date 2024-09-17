143f6cc62e4c59a1fa5441d7e3ffab36254fbe66
Firat Kagitci <firat_kagitci@hotmail.com>
2024-09-17 02:09:39 +0200

    Automated commit on Sal 17.09.2024  2:09:39,85

--- 2. P4_adder/carry_generator_p4.vhd
+++ 2. P4_adder/carry_generator_p4.vhd
@@ -102,7 +102,7 @@ pg_network_: for i in 1 to N generate
  end generate;
 
 	-- Single G block in the first level 
-G_block_1: G_block port map( Pik => p_sig(1), Gk1j => c0, Gik => g_sig(1), Gij => p_sig_1(1)); --This Gij will be one bit in second level of the tree
+G_block_1: G_block port map( Pik => p_sig(1), Gk1j => c0, Gik => g_sig(1), Gij => p_sig_1()); --This Gij will be one bit in second level of the tree
 
 	-- PG blocks in first level
 PG_block_1: for i in 3 to M generate
