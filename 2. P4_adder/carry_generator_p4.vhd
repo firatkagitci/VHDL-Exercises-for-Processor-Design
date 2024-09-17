caf17b1e91c387e4326a0b720452f334d904f428
Firat Kagitci <firat_kagitci@hotmail.com>
2024-09-17 18:13:02 +0200

    Automated commit on Sal 17.09.2024 18:13:02,74

--- 2. P4_adder/carry_generator_p4.vhd
+++ 2. P4_adder/carry_generator_p4.vhd
@@ -85,10 +85,28 @@ end component PG_network;
 	signal g_sig_2: SignalVector_2g;
 
 
+-- fourth level signals
+	constant Z: integer := 4;
+	type SignalVector_3 is array (P downto 2) of std_logic;
+    signal p_sig_3: SignalVector_3;
+	
+	type SignalVector_3g is array (P downto 1) of std_logic;
+	signal g_sig_3: SignalVector_3g;
+	
+-- fifth level signals
+	constant Y: integer := 3;
+	type SignalVector_4 is array (P downto 2) of std_logic;
+    signal p_sig_4: SignalVector_4;
+	
+	type SignalVector_4g is array (P downto 1) of std_logic;
+	signal g_sig_4: SignalVector_4g;
+
+
 
 
 	signal temp1: std_logic;
 	signal temp2: std_logic;
+	signal temp3: std_logic;
 begin
 
     -- Generate PG_network instances --level zero
@@ -118,7 +136,7 @@ PG_block_1: for i in 3 to M generate
 	-- Single G block in the second level 
 G_block_2: G_block port map( Pik => p_sig_1(2), Gk1j => g_sig_1(1) , Gik => g_sig_1(2), Gij => temp1);
 	
-	p_sig_2(1) <= temp1;
+	g_sig_2(1) <= temp1;
 	carry_out(0) <= temp1;
 
 	-- PG blocks in the second level
@@ -134,30 +152,88 @@ PG_block_2: for i in 2 to 7 generate
 		);
 					
 
+
+
 	-- Single G block in the third level 
 G_block_3: G_block port map( Pik => p_sig_2(2), Gk1j => g_sig_2(1) , Gik => g_sig_2(2), Gij => temp2);
 	
-	p_sig_3(1) <= temp2;
+	g_sig_3(1) <= temp2;
 	carry_out(1) <= temp2;
 
-	-- PG blocks in the second level
+	-- PG blocks in the third level
 
-PG_block_3: for i in 2 to 7 generate
-		pgblock: PG_block port map (
-				Gk1j=>g_sig_2(i),
-				Pk1j=>p_sig_2(i),
-				Pik =>p_sig_2(i+1),
-				Gik	=>g_sig_2(i+1),
-				Pij	=>p_sig_3(i),	
-				Gij	=>g_sig_3(i)	
+		pgblock_3_1: PG_block port map (
+				Gk1j=>g_sig_2(3),
+				Pk1j=>p_sig_2(3),
+				Pik =>p_sig_2(4),
+				Gik	=>g_sig_2(4),
+				Pij	=>p_sig_3(2),	
+				Gij	=>g_sig_3(2)	
+		);
+		
+	
+		pgblock_3_2: PG_block port map (
+				Gk1j=>g_sig_2(5),
+				Pk1j=>p_sig_2(5),
+				Pik =>p_sig_2(6),
+				Gik	=>g_sig_2(6),
+				Pij	=>p_sig_3(3),	
+				Gij	=>g_sig_3(3)	
+		);
+		
+		pgblock_3_2: PG_block port map (
+				Gk1j=>g_sig_2(7),
+				Pk1j=>p_sig_2(7),
+				Pik =>p_sig_2(8),
+				Gik	=>g_sig_2(8),
+				Pij	=>p_sig_3(4),	
+				Gij	=>g_sig_3(4)	
 		);
 		
 		
 		
 		
+	-- Two G blocks in the fourth level 
+G_block_4_1: G_block port map( Pik => p_sig_2(3), Gk1j => g_sig_3(1) , Gik => g_sig_2(3), Gij => carry_out(2));
+
+G_block_4_2: G_block port map( Pik => p_sig_3(2), Gk1j => g_sig_3(1) , Gik => g_sig_3(2), Gij => temp3);	
+	
+	carry_out(3) <= temp3;
+	
+	-- Two PG blocks in the fourth level 
 		
 		
-
-
-
+		pgblock_4_1: PG_block port map (
+				Gk1j=>g_sig_3(3),
+				Pk1j=>p_sig_3(3),
+				Pik =>p_sig_2(7),
+				Gik	=>g_sig_2(7),
+				Pij	=>p_sig_4(2),	
+				Gij	=>g_sig_4(2)	
+		);
+			
+				
+		pgblock_4_1: PG_block port map (
+				Gk1j=>g_sig_3(3),
+				Pk1j=>p_sig_3(3),
+				Pik =>p_sig_3(4),
+				Gik	=>g_sig_3(4),
+				Pij	=>p_sig_4(3),	
+				Gij	=>g_sig_4(3)	
+		);
+			
+	
+	-- 4 G blocks in fifth level, last level
+	
+	G_block_5_1: G_block port map( Pik => p_sig_2(5), Gk1j => g_sig_4(1) , Gik => g_sig_2(5), Gij => carry_out(4));
+	G_block_5_2: G_block port map( Pik => p_sig_3(3), Gk1j => g_sig_4(1) , Gik => g_sig_3(3), Gij => carry_out(5));		
+	G_block_5_3: G_block port map( Pik => p_sig_4(2), Gk1j => g_sig_4(1) , Gik => g_sig_4(2), Gij => carry_out(6));	
+	G_block_5_4: G_block port map( Pik => p_sig_4(3), Gk1j => g_sig_4(1) , Gik => g_sig_4(3), Gij => carry_out(7));		
+		
+		
+		
+		
+		
+		
+		
 end architecture structural;
