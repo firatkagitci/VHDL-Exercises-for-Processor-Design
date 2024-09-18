30c408d733d920778331ece4f65b3546f40c3067
Firat Kagitci <firat_kagitci@hotmail.com>
2024-09-19 01:17:25 +0200

    Automated commit on Per 19.09.2024  1:17:25,17

--- 1. carry select-like adder/csla.vhd
+++ 1. carry select-like adder/csla.vhd
@@ -6,7 +6,7 @@ entity csla is
 		
 	port (
 	
-	Cselect		: in std_logic_vector (7 downto 0);
+	Cselect		: in std_logic_vector (6 downto 0);
 	a_32	 	: in std_logic_vector (31 downto 0);
 	b_32 		: in std_logic_vector (31 downto 0);
 	result_32	: out std_logic_vector (31 downto 0);
@@ -35,13 +35,13 @@ begin
 -- instances of csla_block										--Cselect(0) is the carry in
 	
 u1: csla_block port map (a => a_32(3 downto 0),   b => b_32(3 downto 0),   C_sel => CIN		  , Sum => result_32(3 downto 0));
-u2: csla_block port map (a => a_32(7 downto 4),   b => b_32(7 downto 4),   C_sel => Cselect(1), Sum => result_32(7 downto 4));
-u3: csla_block port map (a => a_32(11 downto 8),  b => b_32(11 downto 8),  C_sel => Cselect(2), Sum => result_32(11 downto 8));
-u4: csla_block port map (a => a_32(15 downto 12), b => b_32(15 downto 12), C_sel => Cselect(3), Sum => result_32(15 downto 12)); 
-u5: csla_block port map (a => a_32(19 downto 16), b => b_32(19 downto 16), C_sel => Cselect(4), Sum => result_32(19 downto 16));
-u6: csla_block port map (a => a_32(23 downto 20), b => b_32(23 downto 20), C_sel => Cselect(5), Sum => result_32(23 downto 20));
-u7: csla_block port map (a => a_32(27 downto 24), b => b_32(27 downto 24), C_sel => Cselect(6), Sum => result_32(27 downto 24));
-u8: csla_block port map (a => a_32(31 downto 28), b => b_32(31 downto 28), C_sel => Cselect(7), Sum => result_32(31 downto 28)); 
+u2: csla_block port map (a => a_32(7 downto 4),   b => b_32(7 downto 4),   C_sel => Cselect(0), Sum => result_32(7 downto 4));
+u3: csla_block port map (a => a_32(11 downto 8),  b => b_32(11 downto 8),  C_sel => Cselect(1), Sum => result_32(11 downto 8));
+u4: csla_block port map (a => a_32(15 downto 12), b => b_32(15 downto 12), C_sel => Cselect(2), Sum => result_32(15 downto 12)); 
+u5: csla_block port map (a => a_32(19 downto 16), b => b_32(19 downto 16), C_sel => Cselect(3), Sum => result_32(19 downto 16));
+u6: csla_block port map (a => a_32(23 downto 20), b => b_32(23 downto 20), C_sel => Cselect(4), Sum => result_32(23 downto 20));
+u7: csla_block port map (a => a_32(27 downto 24), b => b_32(27 downto 24), C_sel => Cselect(5), Sum => result_32(27 downto 24));
+u8: csla_block port map (a => a_32(31 downto 28), b => b_32(31 downto 28), C_sel => Cselect(6), Sum => result_32(31 downto 28)); 
 																--Cselect(7) represents carry bit 28
 
 end architecture structural;
--- 2. P4_adder/P4_adder_final.vhd
+++ 2. P4_adder/P4_adder_final.vhd
@@ -25,12 +25,11 @@ end component;
 
 component csla is
 	port (
-	Cselect		: in std_logic_vector  (7  downto 0);
-	a_32	 	: in std_logic_vector  (63 downto 0);
-	b_32 		: in std_logic_vector  (63 downto 0);
-	result_32	: out std_logic_vector (63 downto 0);
+	Cselect		: in std_logic_vector  (6  downto 0);
+	a_32	 	: in std_logic_vector  (31 downto 0);
+	b_32 		: in std_logic_vector  (31 downto 0);
+	result_32	: out std_logic_vector (31 downto 0);
 	CIN			: in std_logic;
-	cout_32		: out std_logic
 	);
 end component;
 
@@ -39,9 +38,9 @@ signal temp6: std_logic_vector (7 downto 0); --index 7 is carry bit 64
 begin
 
 u1 : carry_gen port map(ai => inp1(32 downto 1), bi => inp2(32 downto 1), c0 => carry_in, carry_out => temp5);
-u2 : carry_gen port map(ai => inp1(64 downto 33), bi => inp2(64 downto 33), c0 => temp5(7), carry_out => );
+u2 : carry_gen port map(ai => inp1(64 downto 33), bi => inp2(64 downto 33), c0 => temp5(7), carry_out => temp6);
 
-u3 : csla port map(Cselect => temp5, a_32 => inp1, b_32 => inp2, result_32 => result);
+u3 : csla port map(Cselect => temp5(7 downto 1), a_32 => inp1(32 downto 1), b_32 => inp2(32 downto 1), result_32 => result);
 
 
 end architecture structural;
