4afb35ed6d7b04687ffd42553381f0ee9e37fb9f
Firat Kagitci <firat_kagitci@hotmail.com>
2024-09-19 01:08:34 +0200

    Automated commit on Per 19.09.2024  1:08:34,63

--- 2. P4_adder/P4_adder_final.vhd
+++ 2. P4_adder/P4_adder_final.vhd
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 
 entity p4_adder is 
 port(
-	inp1: in std_logic_vector(32 downto 1);
-	inp2: in std_logic_vector(32 downto 1);
-	result: out std_logic_vector(32 downto 1);
+	inp1: in std_logic_vector(64 downto 1);
+	inp2: in std_logic_vector(64 downto 1);
+	result: out std_logic_vector(64 downto 1);
 	carry_in: in std_logic;
 	carry_o: out std_logic
 	);
@@ -26,19 +26,20 @@ end component;
 component csla is
 	port (
 	Cselect		: in std_logic_vector  (7  downto 0);
-	a_32	 	: in std_logic_vector  (31 downto 0);
-	b_32 		: in std_logic_vector  (31 downto 0);
-	result_32	: out std_logic_vector (31 downto 0);
+	a_32	 	: in std_logic_vector  (63 downto 0);
+	b_32 		: in std_logic_vector  (63 downto 0);
+	result_32	: out std_logic_vector (63 downto 0);
 	CIN			: in std_logic;
 	cout_32		: out std_logic
 	);
 end component;
 
-signal temp5: std_logic_vector (7 downto 1);
+signal temp5: std_logic_vector (7 downto 0); --index 7 is carry bit 32
 
 begin
 
-u1 : carry_gen port map(ai => inp1, bi => inp2, c0 => carry_in, carry_out => temp5);
+u1 : carry_gen port map(ai => inp1(32 downto 1), bi => inp2(32 downto 1), c0 => carry_in, carry_out => temp5);
+u2 : carry_gen port map(ai => inp1(64 downto 33), bi => inp2(64 downto 33), c0 => temp5(7), carry_out =>);
 
 u2 : csla port map(Cselect => temp5, a_32 => inp1, b_32 => inp2, result_32 => result);
 
